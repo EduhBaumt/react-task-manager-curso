b1852b46b790415bd43d6bb763b2a7e2b93d047d
Eduardo Baumart <eduhsouza.baumt@gmail.com>
2025-08-13 12:35:21 -0300

    adição do check icon

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 function Tasks({ task, onTaskClick, onDeleteTaskClick }) {
@@ -16,10 +16,11 @@ function Tasks({ task, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 w-full text-white p-2 rounded-md ${
+            className={`bg-slate-400 w-full text-white p-2 rounded-md flex items-center gap-2 ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <button
